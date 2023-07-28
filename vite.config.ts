c8273d463bc39dc36fb27e0b4110127b58f51468
aadityabhusal <bhusal.001aditya@gmail.com>
2023-07-28 23:46:13 +0530

    feat: uses vite plugin to make the webapp a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: "WordleWiz",
+        name: "WordleWiz - Be a Wordle Wizard",
+        icons: [
+          {
+            src: "/favicon.png",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/png",
+          },
+          {
+            src: "/logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#6aaa64",
+        background_color: "#6aaa64",
+      },
+    }),
+  ],
+});
